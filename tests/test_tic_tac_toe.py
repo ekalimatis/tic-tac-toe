06831d20e09a2574da19a8ca06b50e7c70671012
EKA <eka@limantis.com>
2024-02-25 19:14:14 +0300

    change player and function check_position

--- tests/test_tic_tac_toe.py
+++ tests/test_tic_tac_toe.py
@@ -3,71 +3,93 @@ import pytest
 from tic_tac_toe import TicTacToe
 
 
-def test_check_position():
+# def test_check_position():
+#     game = TicTacToe()
+#     game._field = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+#     game._check_position()
+#     assert game.game_status == 'Next turn'
+#
+#     game._field = [[1, 0, 0], [1, 0, 0], [1, 0, 0]]
+#     game._check_position()
+#     assert game.game_status == 'X win!'
+#
+#     game._field = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
+#     game._check_position()
+#     assert game.game_status == 'X win!'
+#
+#     game._field = [[0, 1, 0], [0, 1, 0], [0, 1, 0]]
+#     game._check_position()
+#     assert game.game_status == 'X win!'
+#
+#     game._field = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+#     game._check_position()
+#     assert game.game_status == 'X win!'
+#
+#     game._field = [[0, 0, 1], [0, 1, 0], [1, 0, 0]]
+#     game._check_position()
+#     assert game.game_status == 'X win!'
+#
+#     game._field = [[1, 1, 1], [4, 4, 0], [4, 4, 1]]
+#     game._check_position()
+#     assert game.game_status == 'X win!'
+#
+#     game._field = [[4, 4, 4], [4, 4, 4], [4, 4, 4]]
+#     game._check_position()
+#     assert game.game_status == '0 win!'
+#
+#     game._field = [[0, 4, 0], [0, 4, 0], [0, 4, 0]]
+#     game._check_position()
+#     assert game.game_status == '0 win!'
+#
+#     game._field = [[4, 0, 0], [0, 4, 0], [0, 0, 4]]
+#     game._check_position()
+#     assert game.game_status == '0 win!'
+#
+#     game._field = [[4, 0, 0],
+#                    [0, 4, 0],
+#                    [0, 0, 4]]
+#     game._check_position()
+#     assert game.game_status == '0 win!'
+#
+#     game._field = [[0, 0, 4],
+#                    [0, 4, 0],
+#                    [4, 0, 0]]
+#     game._check_position()
+#     assert game.game_status == '0 win!'
+#
+#     game._field = [[4, 1, 4],
+#                    [1, 1, 4],
+#                    [4, 4, 1]]
+#     game._check_position()
+#     assert game.game_status == 'Game over. Draw.'
+
+def test_winner_by_turn():
     game = TicTacToe()
-    game._field = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-    game._check_position()
-    assert game.game_status == 'Next turn'
+    game._field = [[3, 1, 4],
+                   [1, 3, 4],
+                   [4, 4, 3]]
+    assert game._check_winner_by_turn((1,1)) == True
 
-    game._field = [[1, 0, 0], [1, 0, 0], [1, 0, 0]]
-    game._check_position()
-    assert game.game_status == 'X win!'
+    game._field = [[0, 1, 4],
+                   [0, 3, 4],
+                   [0, 4, 1]]
+    assert game._check_winner_by_turn((2,0)) == True
 
-    game._field = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
-    game._check_position()
-    assert game.game_status == 'X win!'
+    game._field = [[9, 1, 5],
+                   [7, 5, 4],
+                   [5, 4, 1]]
+    assert game._check_winner_by_turn((0,2)) == True
 
-    game._field = [[0, 1, 0], [0, 1, 0], [0, 1, 0]]
-    game._check_position()
-    assert game.game_status == 'X win!'
 
-    game._field = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
-    game._check_position()
-    assert game.game_status == 'X win!'
-
-    game._field = [[0, 0, 1], [0, 1, 0], [1, 0, 0]]
-    game._check_position()
-    assert game.game_status == 'X win!'
-
-    game._field = [[1, 1, 1], [4, 4, 0], [4, 4, 1]]
-    game._check_position()
-    assert game.game_status == 'X win!'
-
-    game._field = [[4, 4, 4], [4, 4, 4], [4, 4, 4]]
-    game._check_position()
-    assert game.game_status == '0 win!'
-
-    game._field = [[0, 4, 0], [0, 4, 0], [0, 4, 0]]
-    game._check_position()
-    assert game.game_status == '0 win!'
-
-    game._field = [[4, 0, 0], [0, 4, 0], [0, 0, 4]]
-    game._check_position()
-    assert game.game_status == '0 win!'
-
-    game._field = [[4, 0, 0],
-                   [0, 4, 0],
-                   [0, 0, 4]]
-    game._check_position()
-    assert game.game_status == '0 win!'
-
-    game._field = [[0, 0, 4],
-                   [0, 4, 0],
-                   [4, 0, 0]]
-    game._check_position()
-    assert game.game_status == '0 win!'
-
-    game._field = [[4, 1, 4],
-                   [1, 1, 4],
-                   [4, 4, 1]]
-    game._check_position()
-    assert game.game_status == 'Game over. Draw.'
 
 def test_make_turn():
     game = TicTacToe()
-    game._field = [[0, 1, 4], [1, 4, 1], [4, 1, 1]]
-    assert game._make_turn() == (0, 0)
+    game._field = [[' ', 1, 4], [1, 4, 1], [4, 1, 1]]
+    assert game._computer_turn() == (0, 0)
 
-    game._field = [[1, 1, 4], [1, 0, 1], [4, 1, 1]]
-    assert game._make_turn() == (1, 1)
+    game._field = [[1, 1, 4], [1, ' ', 1], [4, 1, 1]]
+    assert game._computer_turn() == (1, 1)
+
+def test_console_display():
+    pass
 
--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,127 +1,140 @@
 import random
 from enum import Enum
 
+class ValidationTurnError(Exception):
+    pass
 
-class Player(Enum):
-    EMPTY = 0
-    X = 1
-    O = 4
+class FormatTurnError(ValidationTurnError):
+    pass
 
+class RangeTurnError(ValidationTurnError):
+    pass
+
+class CellAlreadyFillError(ValidationTurnError):
+    pass
+
+class Player:
+    EMPTY = ' '
+    X = 'X'
+    O = '0'
+
+def consol_display(field: list[list]) -> None:
+    field_for_print = ''
+    for row in field:
+        row_for_print = ''
+        for cell in row:
+            cell_for_print = '|' + cell
+            row_for_print += cell_for_print
+        row_for_print += '|\n'
+        field_for_print += row_for_print
+    print(field_for_print)
 
 class TicTacToe:
-    cell_mask = {
-        Player.EMPTY.value: ' ',
-        Player.X.value: 'X',
-        Player.O.value: 'O'
-    }
+#     cell_mask = {
+#         Player.EMPTY.value: ' ',
+#         Player.X.value: 'X',
+#         Player.O.value: 'O'
+#     }
 
-    def __init__(self):
-        self._field = [[Player.EMPTY.value for _ in range(3)] for _ in range(3)]
+    def __init__(self) -> None:
+        self._field = [[Player.EMPTY for _ in range(3)] for _ in range(3)]
         self._turns_count = 0
         self.game_status = 'Next turn'
         self._is_over = False
         self._my_turn = random.choice([True, False])
 
-    def __repr__(self):
-        field_for_print = ''
-        for row in self._field:
-            row_for_print = ''
-            for cell in row:
-                cell_for_print = '|' + TicTacToe.cell_mask[cell]
-                row_for_print += cell_for_print
-            row_for_print += '|\n'
-            field_for_print += row_for_print
-        return field_for_print
-
-    def set_turn(self, turn: tuple[int, int], player: Player) -> bool:
+    def _validate_turn(self, turn: str) -> tuple[int, int]:
         try:
-            row, col = int(turn[0]), int(turn[1])
+            row, col = list(int(_) for _ in turn.split(','))
         except ValueError:
-            print('Wrong turn, col and row must be >= 0 and <= 2')
-            return False
+            raise FormatTurnError
         if not 0 <= row <= 2 or not 0 <= row <= 2:
-            print('Wrong turn, col and row must be >= 0 and <= 2')
-            return False
-        if self._field[row][col] == Player.EMPTY.value:
-            self._field[row][col] = player
+            raise RangeTurnError
+        if self._field[row][col] != Player.EMPTY:
+            raise CellAlreadyFillError
+        return row, col
+
+    def set_turn(self, turn: tuple[int, int], player: Player) -> None:
+        row, col = turn
+        self._field[row][col] = player
+
+    def _check_winner_by_turn(self, turn: tuple[int, int]) -> bool:
+        row, col = turn
+        if len(set(self._field[row])) == 1:
             return True
-        print('Wrong turn, cell is fill!')
+
+        if len(set([_[col] for _ in self._field])) == 1:
+            return True
+
+        if row == col and len(set([self._field[n][n] for n, _ in enumerate(self._field)])) == 1:
+            return True
+
+        if row + col == len(self._field) - 1 and len(set([self._field[n][len(self._field) - n - 1] for n, _ in enumerate(self._field)])) == 1:
+            return True
+
         return False
 
-    def _check_position(self) -> None:
-        colums_sum = [0, 0, 0]
-        left_to_right = 0
-        right_to_left = 0
-        for col, row in enumerate(self._field):
-            if sum(row) == 3:
-                self._is_over = True
-                self.game_status = 'X win!'
-                return
-            if sum(row) == 12:
-                self._is_over = True
-                self.game_status = '0 win!'
-                return
-            colums_sum[0] += row[0]
-            colums_sum[1] += row[1]
-            colums_sum[2] += row[2]
-            left_to_right += row[col]
-            right_to_left += row[len(row) - 1 - col]
-        for sum_col in colums_sum:
-            if sum_col == 3 or left_to_right == 3 or right_to_left == 3:
-                self._is_over = True
-                self.game_status = 'X win!'
-                return
-            if sum_col == 12 or left_to_right == 12 or right_to_left == 12:
-                self._is_over = True
-                self.game_status = '0 win!'
-                return
-        if self._turns_count == 9:
-            self._is_over = True
-            self.game_status = 'Game over. Draw.'
-            return
-        self.game_status = 'Next turn'
-        return
-
-    def _make_turn(self) -> tuple[int, int]:
+    def _get_free_cells(self):
         free_cells = []
         for row, _ in enumerate(self._field):
             for col, _ in enumerate(_):
-                if self._field[row][col] == Player.EMPTY.value:
+                if self._field[row][col] == Player.EMPTY:
                     free_cells.append((row, col))
-        turn = random.choice(free_cells)
+        return free_cells
+
+    def _computer_turn(self) -> tuple[int, int]:
+        turn = random.choice(self._get_free_cells())
+        return turn
+
+    def _player_turn(self):
+        is_correct_turn = False
+        while not is_correct_turn:
+            player_turn = input('Enter your turn in following format <row>,<column>: ')
+            try:
+                turn = self._validate_turn(player_turn)
+            except (FormatTurnError, RangeTurnError):
+                print('Wrong turn, col and row must be >= 0 and <= 2')
+            except CellAlreadyFillError:
+                print('Cell already fill, choose another turn.')
+            else:
+                is_correct_turn = True
         return turn
 
     def run(self) -> None:
         player = Player.X
         print('Game start')
-        print(self)
+        consol_display(self._field)
         while not self._is_over:
             if self._my_turn:
                 print('My turn!')
             else:
                 print('Your turn!')
             if self._my_turn:
-                self.set_turn(self._make_turn(), player.value)
+                turn = self._computer_turn()
             else:
-                is_correct_turn = False
-                while not is_correct_turn:
-                    print('Enter your turn in following format <row>,<column>: ', end='')
-                    is_correct_turn = self.set_turn(input().split(','), player.value)
-            self._turns_count += 1
-            self._check_position()
+                turn = self._player_turn()
+            self.set_turn(turn, player)
+            consol_display(self._field)
+            if self._check_winner_by_turn(turn):
+                print(f'{player} win!')
+                break
+            if not self._get_free_cells():
+                print('Draw!')
+            # self._turns_count += 1
             self._my_turn = not self._my_turn
             if player == Player.X:
                 player = Player.O
             else:
                 player = Player.X
-            print(self)
-        print(self.game_status)
 
-
-    def start_new_game(self):
-        pass
-
+def main():
+    one_more_game = True
+    while one_more_game:
+        game = TicTacToe()
+        game.run()
+        answer = input('One more game y/n?')
+        if answer != 'y':
+            one_more_game = False
 
 if __name__ == '__main__':
-    game = TicTacToe()
-    game.run()
+    main()
